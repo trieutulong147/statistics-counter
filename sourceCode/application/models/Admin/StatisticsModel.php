5126f2dda7084516fb8aaff10ba874ffcc1c9ae0
trieutulong147 <trieutulong1407@gmail.com>
2016-11-07 10:49:36 +0700

    Make some changes with model and config

--- sourceCode/application/models/Admin/StatisticsModel.php
+++ sourceCode/application/models/Admin/StatisticsModel.php
@@ -1,4 +1,10 @@
 <?php
+/*
+* Class for optimization
+* Author: TTL
+* Create time: 06/11/2016
+* Update time: 06/11/2016
+*/
 class Model_Admin_StatisticsModel extends Zend_Db_Table_Abstract {
 	protected $_name = 'statistics';
     protected $_primary = 'id';
